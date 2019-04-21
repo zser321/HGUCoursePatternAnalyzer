1de0a75f108f46ed4f6d304826ae3797b6cfd52b
JC <jc@lifove.net>
2019-04-21 17:40:14 +0900

    Initial commit

---
+++ src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
@@ -0,0 +1,14 @@
+package edu.handong.analysis;
+
+public class HGUCoursePatternAnalyzer {
+
+	public static void main(String[] args) {
+		HGUCoursePatternAnalyzer analyzer = new HGUCoursePatternAnalyzer();
+		analyzer.run(args);
+	}
+
+	private void run(String[] args) {
+		
+	}
+
+}
