627e7218c58f7f4049b6907e966ea3f3a277055d
JC <jc@lifove.net>
2019-04-21 23:13:13 +0900

    Update HGUCoursePatternAnalyzer

--- src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
+++ src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
@@ -1,14 +1,76 @@
 package edu.handong.analysis;
 
-public class HGUCoursePatternAnalyzer {
+import edu.handong.analysis.datamodel.Course;
+import edu.handong.analysis.datamodel.Student;
 
-	public static void main(String[] args) {
-		HGUCoursePatternAnalyzer analyzer = new HGUCoursePatternAnalyzer();
-		analyzer.run(args);
+public class HGUCoursePatternAnalyzer {
+	
+	String[] lines = {	"1999-1, JC Nam, Java Programming",
+						"1999-2, JC Nam, Programming Language Theory",
+						"1999-1, JC Nam, Data Structures",
+						"2001-1, JC Nam, Database Systems",
+						"2018-1, SB Lim, Java Programming",
+						"2018-2, SB Lim, Programming Language Theory",
+						"2019-1, SB Lim, Data Structures",
+						"2019-1, SB Lim, Algorithm Analysis",
+						"2018-1, SJ Kim, Java Programming",
+						"2018-2, SJ Kim, Programming Language Theory",
+						"2019-1, SJ Kim, Logic Design",
+						"2019-1, SJ Kim, Algorithm Analysis",
+						};
+
+	int numOfStudents;
+	int numOfCourses;
+	Student[] students;
+	Course[] courses;
+	
+	public void run(String[] args) {
+		
+		numOfStudents = Integer.parseInt(args[0]);
+		numOfCourses = Integer.parseInt(args[1]);
+	
+		students = initiateStudentArrayFromLines(lines);
+		
+		System.out.println("Number of All Students: " + numOfStudents);
+		for(Student student: students) {
+			System.out.println(student.getName());
+		}
+		
+		courses = initiateCourseArrayFromLines(lines);
+		System.out.println("Number of All Courses: " + numOfCourses);
+		for(Course course: courses) {
+			System.out.println(course.getCourseName());
+		}
+		
 	}
 
-	private void run(String[] args) {
+	private Student[] initiateStudentArrayFromLines(String[] lines) {
 		
+		// TODO: implement this method
+		
+		
+		return null;
+	}
+
+	private boolean studentExist(Student[] students, Student student) {
+		
+		// TODO: implement this method
+
+		return false;
+	}
+	
+	private Course[] initiateCourseArrayFromLines(String[] lines) {
+		
+		// TODO: implement this method
+		
+		return null;
+	}
+
+	private boolean courseExist(Course[] courses, Course course) {
+		
+		// TODO: implement this method
+
+		return false;
 	}
 
 }
